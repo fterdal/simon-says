ca3d816ffc679aaf924df9d324d8e43a6cc80188
Finn Terdal <finnterdal@gmail.com>
2021-01-21 16:53:20 -0600

    adds color button component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ColorButtonComponent } from './color-button/color-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ColorButtonComponent
   ],
   imports: [
     BrowserModule
