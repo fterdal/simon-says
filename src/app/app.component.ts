0c93a70fa23fe59694512044559ba4551c16e9cf
Finn Terdal <finnterdal@gmail.com>
2021-01-25 17:28:01 -0600

    messing with Input passing data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
 
 // TODO: How to assign a css property to the host node???
 export class AppComponent {
-  colors = ['blue', 'yellow', 'green', 'red']
+  colors = ['blue', 'yellow', 'green', 'red'];
+  // TODO: What happens when the child component changes it's color?
+  // Does the App component register a change? Or is it singlely bound?
 }
--- src/app/color-button/color-button.component.ts
+++ src/app/color-button/color-button.component.ts
@@ -9,10 +9,14 @@ import { Component, Input } from '@angular/core';
 // "Property 'color' has no initializer and is not definitely assigned in the constructor"
 // SO suggests Adding "strictPropertyInitialization": true to your tsconfig.json, but maybe that's bad?
 // https://stackoverflow.com/questions/49699067/property-has-no-initializer-and-is-not-definitely-assigned-in-the-construc
+
+// Okay, this article actually has some helpful references:
+// https://ultimatecourses.com/blog/passing-data-angular-2-components-input
 export class ColorButtonComponent {
   @Input() color: string;
 
-  constructor() {
-    console.log(this);
+  changeColor() {
+    console.log("color change")
+    this.color = 'purple'
   }
 }
