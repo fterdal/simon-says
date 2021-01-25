53c1a32c845edefcd3e2ec19b51512356d11fbc3
Finn Terdal <finnterdal@gmail.com>
2021-01-25 17:06:55 -0600

    moves some html to color-button component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
+
+// TODO: How to assign a css property to the host node???
 export class AppComponent {
-  title = 'simon-says';
+  colors = ['blue', 'yellow', 'green', 'red']
 }
--- src/app/color-button/color-button.component.ts
+++ src/app/color-button/color-button.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-color-button',
+  selector: 'color-button',
   templateUrl: './color-button.component.html',
-  styleUrls: ['./color-button.component.scss']
+  styleUrls: ['./color-button.component.scss'],
 })
-export class ColorButtonComponent implements OnInit {
+// TODO: Typescript is saying:
+// "Property 'color' has no initializer and is not definitely assigned in the constructor"
+// SO suggests Adding "strictPropertyInitialization": true to your tsconfig.json, but maybe that's bad?
+// https://stackoverflow.com/questions/49699067/property-has-no-initializer-and-is-not-definitely-assigned-in-the-construc
+export class ColorButtonComponent {
+  @Input() color: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+    console.log(this);
   }
-
 }
